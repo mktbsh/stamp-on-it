f08b3f860fe82d63edae233dc247890f4f277c88
mktbsh <t93.hasebe@gmail.com>
2024-02-05 00:12:44 +0900

    update

--- src/screen/BinarizedImage.tsx
+++ src/screen/BinarizedImage.tsx
@@ -1,22 +1,42 @@
-import { Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import {
+  Flex,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
 import { PositiveButton, ResetButton } from "./_components/buttons";
 
 type Props = {
   file: Blob;
 };
 
+type State = {
+  filename: string;
+  image: string;
+};
+
 export function BinarizedImage({ file }: Props) {
-  const [imageSource, setImageSource] = useState<string>("");
+  const [state, setState] = useState<State>({ filename: "", image: "" });
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    const revoke = (v: string) => URL.revokeObjectURL(v);
+
     const url = URL.createObjectURL(file);
 
-    setImageSource((prev) => {
-      if (prev) {
-        URL.revokeObjectURL(prev);
-      }
-      return url;
+    setState((prev) => {
+      prev.image && revoke(prev.image);
+
+      return {
+        filename: window.crypto.randomUUID(),
+        image: url,
+      };
     });
 
     return () => {
@@ -24,12 +44,14 @@ export function BinarizedImage({ file }: Props) {
     };
   }, [file]);
 
-  if (!imageSource) return null;
+  if (!state.image) return null;
 
   function handleDownload() {
+    const filename = inputRef.current?.value || state.filename;
+
     const a = document.createElement("a");
-    a.href = imageSource;
-    a.download = `${Date.now()}.jpeg`;
+    a.href = state.image;
+    a.download = `${filename}.png`;
     a.click();
     a.remove();
   }
@@ -43,7 +65,11 @@ export function BinarizedImage({ file }: Props) {
       </Stack>
       <Stack spacing="6">
         <Stack spacing="4" maxW="full">
-          <Image src={imageSource} alt="" />
+          <Image src={state.image} alt="" />
+          <InputGroup>
+            <Input ref={inputRef} type="text" placeholder={state.filename} />
+            <InputRightAddon>.png</InputRightAddon>
+          </InputGroup>
         </Stack>
       </Stack>
       <Text textStyle="sm" color="fg.muted" textAlign="center">
